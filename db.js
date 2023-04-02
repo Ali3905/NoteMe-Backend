8c8f3f93d08d085056f324cfcdce832c560fd2ba
Ali3905 <muhammadali30905@gmail.com>
2023-04-02 06:05:43 +0500

    connecting Database

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/persons"
+const mongoURI = "mongodb+srv://muhammadali30905:aliahmed3905@noteme.7lqbkgb.mongodb.net/NoteMe?retryWrites=true&w=majority"
 
 const mongoToConnect = () => {
     mongoose.connect(mongoURI, ()=>{
