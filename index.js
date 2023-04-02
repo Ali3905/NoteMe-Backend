fbc9e9526e7e4c54794b8f860db4c3a89389943d
Ali3905 <muhammadali30905@gmail.com>
2023-04-02 20:45:26 +0500

    cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000
 
 app.use(express.json())
 app.use(cors({
- origin: ["http://localhost:3000", "https://ali3905.github.io"]
+ origin: 'http://localhost:3000'
 }))
 
 app.use("/api/auth", require("./routes/users"))
