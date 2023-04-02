fbbd4a955fef87c31e88b4fc00bb2dd0259333b8
Ali3905 <muhammadali30905@gmail.com>
2023-04-02 21:22:01 +0500

    cors

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
-app.use(cors({
- origin: 'http://localhost:3000/NoteMe'
-}))
+app.use(cors())
 
 app.use("/api/auth", require("./routes/users"))
 app.use("/api/notes", require("./routes/notes"))
