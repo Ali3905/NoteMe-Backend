bebad34f12c05baee154096be09e14cb1a7df00f
Ali3905 <muhammadali30905@gmail.com>
2023-04-02 21:18:07 +0500

    dsfa

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000
 
 app.use(express.json())
 app.use(cors({
- origin: 'http://localhost:3000'
+ origin: 'http://localhost:3000/NoteMe'
 }))
 
 app.use("/api/auth", require("./routes/users"))
